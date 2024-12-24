cc6ce4b3957d95436a1a31fc76fc3ec8f5d3ae37
Soltan <mahmudovsoltan477@gmail.com>
2024-12-25 00:45:59 +0400

    fix select bag

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,13 @@ import HomeWork from "./pages/homework/HomeWork";
 import HWResalt from './pages/homeWorkResalt/HWResalt';
 
 
+
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/classes" element={<Classes />} />
+      {/* <Route path="/classes" element={<Classes />} /> */}
       <Route path="/exams" element={<Exams />} />
       <Route path="/exams-results" element={<Exams />} />
       <Route path="/homework" element={<HomeWork />} />
